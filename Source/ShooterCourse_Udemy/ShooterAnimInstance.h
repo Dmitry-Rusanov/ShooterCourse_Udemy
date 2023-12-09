83414b30ec354b3e1cefba3960dc11023ccdf106
Dmitry <55001381+Dmitry-Rusanov@users.noreply.github.com>
2023-12-09 14:48:31 +0300

    1

--- Source/ShooterCourse_Udemy/ShooterAnimInstance.h
+++ Source/ShooterCourse_Udemy/ShooterAnimInstance.h
@@ -38,7 +38,9 @@ private:
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Movement", meta=(AllowPrivateAccess = "true"))
 	bool bIsAccelerating;
 
-
+	/** Смещение рысканья которое используется для стрейфа */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Movement", meta=(AllowPrivateAccess = "true"))
+	float MovementOffset;
 	
 	
 	
